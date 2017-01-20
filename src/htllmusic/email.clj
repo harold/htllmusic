f0482e4dadfe0223ca1cf5af14b05ad1927691ac
Harold <hhausman@gmail.com>
2017-01-19 21:02:43 -0700

    [email] Read secrets from resources config.

--- src/htllmusic/email.clj
+++ src/htllmusic/email.clj
@@ -1,11 +1,25 @@
 (ns htllmusic.email
-  (:require [org.httpkit.client :as http]))
+  (:require [clojure.java.io :as io]
+            [org.httpkit.client :as http]))
+
+(def email-config* (atom nil))
+
+(defn- populate-config
+  []
+  (->> "email-config.edn"
+       (io/resource)
+       (slurp)
+       (clojure.edn/read-string)
+       (reset! email-config*)))
 
 (defn send-email
   [to subject body]
-  (http/post "https://api.mailgun.net/v3/<DOMAIN>/messages"
-             {:basic-auth ["api" "<KEY>"]
-              :form-params {:from "admin@htllmusic.com"
-                            :to to
-                            :subject subject
-                            :html body}}))
+  (if-not @email-config* (populate-config))
+  (let [domain (:domain @email-config*)
+        email-key (:key @email-config*)]
+    (http/post (str "https://api.mailgun.net/v3/" domain "/messages")
+               {:basic-auth ["api" email-key]
+                :form-params {:from "admin@htllmusic.com"
+                              :to to
+                              :subject subject
+                              :html body}})))
